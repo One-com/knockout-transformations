be8536408e746ff44e476040f0fc6ae198231f97
Sune Simonsen <sune@we-knowhow.dk>
2015-01-02 22:48:39 +0100

    Fixed problem where sortingKeysEquals always would return false.
    Problem found by @AdamWillden

--- src/knockout-projections.js
+++ src/knockout-projections.js
@@ -373,7 +373,7 @@ See the Apache Version 2.0 License for specific language governing permissions a
             }
         }
 
-        return false;
+        return true;
     }
 
     function compareSortingKeys(aSortKeys, bSortKeys) {
