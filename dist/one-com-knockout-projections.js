60ac72b1d434c6b7b4f3720bc0a26c1831ef56fc
Sune Simonsen <sune@we-knowhow.dk>
2015-01-02 22:50:01 +0100

    Release 1.3.1

--- dist/one-com-knockout-projections.js
+++ dist/one-com-knockout-projections.js
@@ -1,4 +1,4 @@
-/*! Knockout projections plugin - version 1.3.0
+/*! Knockout projections plugin - version 1.3.1
 ------------------------------------------------------------------------------
 Copyright (c) Microsoft Corporation
 All rights reserved.
@@ -373,7 +373,7 @@ See the Apache Version 2.0 License for specific language governing permissions a
             }
         }
 
-        return false;
+        return true;
     }
 
     function compareSortingKeys(aSortKeys, bSortKeys) {
